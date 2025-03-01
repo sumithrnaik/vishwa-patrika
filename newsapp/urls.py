a35d805f6d931416443291e1f04e64625fd9932b
sumithrnaik <sumithnaik1072003@gmail.com>
2025-03-01 21:21:57 +0530

    second commit

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import news_feed,fetch_and_store_data
 
 urlpatterns = [
-    # path('',fetch_and_store_data,name='fetch_and_store_data'),
-    path('news/', news_feed, name='news_feed'),
+    path('',fetch_and_store_data,name='fetch_and_store_data'),
+    path('', news_feed, name='news_feed'),
 ]
